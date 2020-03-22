592e114ac803111512388d26edef137d92045eb9
Kirk Scheibelhut <kjs@scheibo.com>
2020-03-22 13:13:33 -0700

    Reorg for verifier

--- protocol/jest.config.js
+++ protocol/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testPathIgnorePatterns: ['node_modules/', 'index.test.js'],
+  testPathIgnorePatterns: ['node_modules/', 'build/'],
 };
