cf933234780df5c6b4357313d3ab73b2a355cec5
Kirk Scheibelhut <kjs@scheibo.com>
2021-05-02 11:43:51 -0700

    Fix Utils sort

--- randoms/src/utils.ts
+++ randoms/src/utils.ts
@@ -36,7 +36,7 @@ function compare(a: Comparable, b: Comparable): number {
 		}
 		return 0;
 	}
-	if (a.reverse) {
+  if ('reverse' in a) {
 		return compare((b as {reverse: string}).reverse, a.reverse);
 	}
 	throw new Error(`Passed value ${a} is not comparable`);
