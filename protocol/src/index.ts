28bf0ffe5b513116236f54688dfded1c6bd3e492
taylorhansen <taylor@hansenfamily.us>
2021-05-31 19:06:25 -0700

    Add arg typigns

--- protocol/src/index.ts
+++ protocol/src/index.ts
@@ -651,6 +651,21 @@ export namespace Protocol {
     '|controlshtml|': readonly ['controlshtml', HTML];
     '|fieldhtml|': readonly ['fieldhtml', HTML];
     '|debug|': readonly ['debug', Message];
+    '|deinit|': readonly ['deinit'];
+    '|selectorhtml|': readonly ['selectorhtml', string, HTML];
+    '|refresh|': readonly ['refresh'];
+    '|tempnotify|': readonly ['tempnotify', string, Message, ...[] | [Message] | [Message, string]];
+    '|tempnotifyoff|': readonly ['tempnotifyoff', string];
+    '|noinit|namerequired|': readonly ['noinit', 'namerequired', Message];
+    '|noinit|nonexistent|': readonly ['noinit', 'nonexistent', Message];
+    '|noinit|joinfailed|': readonly ['noinit', 'joinfailed', Message];
+    '|noinit|rename|': readonly ['noinit', 'rename', string, string];
+    '|hidelines|delete|': readonly ['hidelines', 'delete', string, Num];
+    '|hidelines|hide|': readonly ['hidelines', 'hide', string, Num];
+    '|hidelines|unlink|': readonly ['hidelines', 'unlink', string];
+    '|expire|': readonly ['expire', Message?];
+    '|modaction|': readonly ['modaction', Message];
+    '|askreg|': readonly ['askreg', string];
   }
 
   export interface TournamentArgs {
@@ -1803,18 +1818,22 @@ export const Protocol = new class {
     '|nametaken|': 1, '|challstr|': 1, '|updateuser|': 1, '|formats|': 1, '|updatesearch|': 1,
     '|message|': 1, '|updatechallenges|': 1, '|queryresponse|': 1, '|unlink|': 1, '|raw|': 1,
     '|error|': 1, '|bigerror|': 1, '|chatmsg|': 1, '|chatmsg-raw|': 1, '|controlshtml|': 1,
-    '|fieldhtml|': 1, '|debug|': 1, '|tournament|create|': 1, '|tournament|update|': 1,
-    '|tournament|updateEnd|': 1, '|tournament|error|': 1, '|tournament|forceend|': 1,
-    '|tournament|join|': 1, '|tournament|leave|': 1, '|tournament|replace|': 1,
-    '|tournament|start|': 1, '|tournament|disqualify|': 1, '|tournament|battlestart|': 1,
-    '|tournament|battleend|': 1, '|tournament|end|': 1, '|tournament|scouting|': 1,
-    '|tournament|autostart|': 1, '|tournament|autodq|': 1, '|player|': 1, '|teamsize|': 1,
-    '|gametype|': 1, '|gen|': 1, '|tier|': 1, '|rated|': 1, '|seed|': 1, '|rule|': 1, '|split|': 1,
-    '|teampreview|': 1, '|clearpoke|': 1, '|poke|': 1, '|start|': 1, '|done|': 1, '|request|': 1,
-    '|inactive|': 1, '|inactiveoff|': 1, '|upkeep|': 1, '|turn|': 1, '|win|': 1, '|tie|': 1,
-    '|move|': 1, '|switch|': 1, '|drag|': 1, '|detailschange|': 1, '|replace|': 1, '|swap|': 1,
-    '|cant|': 1, '|faint|': 1, '|-formechange|': 1, '|-fail|': 1, '|-block|': 1, '|-notarget|': 1,
-    '|-miss|': 1, '|-damage|': 1, '|-heal|': 1, '|-sethp|': 1, '|-status|': 1, '|-curestatus|': 1,
+    '|fieldhtml|': 1, '|debug|': 1, '|deinit|': 1, '|selectorhtml|': 1, '|refresh|': 1,
+    '|tempnotify|': 1, '|tempnotifyoff|': 1, '|noinit|namerequired|': 1, '|noinit|nonexistent|': 1,
+    '|noinit|joinfailed|': 1, '|noinit|rename|': 1, '|hidelines|delete|': 1, '|hidelines|hide|': 1,
+    '|hidelines|unlink|': 1, '|expire|': 1, '|modaction|': 1, '|askreg|': 1,
+    '|tournament|create|': 1, '|tournament|update|': 1, '|tournament|updateEnd|': 1,
+    '|tournament|error|': 1, '|tournament|forceend|': 1, '|tournament|join|': 1,
+    '|tournament|leave|': 1, '|tournament|replace|': 1, '|tournament|start|': 1,
+    '|tournament|disqualify|': 1, '|tournament|battlestart|': 1, '|tournament|battleend|': 1,
+    '|tournament|end|': 1, '|tournament|scouting|': 1, '|tournament|autostart|': 1,
+    '|tournament|autodq|': 1, '|player|': 1, '|teamsize|': 1, '|gametype|': 1, '|gen|': 1,
+    '|tier|': 1, '|rated|': 1, '|seed|': 1, '|rule|': 1, '|split|': 1, '|teampreview|': 1,
+    '|clearpoke|': 1, '|poke|': 1, '|start|': 1, '|done|': 1, '|request|': 1, '|inactive|': 1,
+    '|inactiveoff|': 1, '|upkeep|': 1, '|turn|': 1, '|win|': 1, '|tie|': 1, '|move|': 1,
+    '|switch|': 1, '|drag|': 1, '|detailschange|': 1, '|replace|': 1, '|swap|': 1, '|cant|': 1,
+    '|faint|': 1, '|-formechange|': 1, '|-fail|': 1, '|-block|': 1, '|-notarget|': 1, '|-miss|': 1,
+    '|-damage|': 1, '|-heal|': 1, '|-sethp|': 1, '|-status|': 1, '|-curestatus|': 1,
     '|-cureteam|': 1, '|-boost|': 1, '|-unboost|': 1, '|-setboost|': 1, '|-swapboost|': 1,
     '|-invertboost|': 1, '|-clearboost|': 1, '|-clearallboost|': 1, '|-clearpositiveboost|': 1,
     '|-ohko|': 1, '|-clearnegativeboost|': 1, '|-copyboost|': 1, '|-weather|': 1,
