3a7ae8d9a16234ae84809d7d430d8259f75e8367
Sergio Garcia <sergio@garciadelacruz.es>
2025-04-30 17:34:01 +0200

    Fix exports

--- img/src/index.ts
+++ img/src/index.ts
@@ -4,4 +4,9 @@ import {Data} from './data';
 export const Sprites = new A.Sprites(Data);
 export const Icons = new A.Icons(Data);
 
-export {GraphicsGen, AnimatedGraphicsGen, SecondFrameGraphicsGen, PokemonSprite} from './adaptable';
+export {
+  type GraphicsGen,
+  type AnimatedGraphicsGen,
+  type SecondFrameGraphicsGen,
+  type PokemonSprite,
+} from './adaptable';
